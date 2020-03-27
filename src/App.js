4b9e156547ba52ca9db944bd7624c37db142c264
Rachele Edwards <dot.com1472@gmail.com>
2020-03-27 09:27:27 -0600

    initial set up on home.js and form.js. Installed all needed libraries and did form skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import {Route, Link} from "react-router-dom"
+import PizzaForm from "./components/PizzaForm"
+import Home from "./components/Home"
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      <button>
+        <Route exact path = "/">
+          <Home/>
+        </Route>
+        <Route path ="/pizza">
+          <PizzaForm/>
+        </Route>
+      </button>
     </>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {useHistory} from "react-router-dom"
+
+function Home() {
+    const goBack = useHistory()
+    console.log("go back", goBack)
+
+    const buyPizza = () => {
+        console.log("Submitting...")
+        goBack.push("/pizza")
+    }
+    return (
+        <button onClick={buyPizza}>Order Now!</button>
+    )
+}
+
+export default Home
